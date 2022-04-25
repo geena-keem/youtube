e21752b71a4e81c1c639f717c85b5122282104c1
geena-keem <geena.gamm@gmail.com>
2022-04-25 22:53:06 +0900

    Concurrent Mode

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -13,11 +13,14 @@ const httpClient = axios.create({
 
 const youtube = new Youtube(httpClient);
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
     <React.StrictMode>
         <App youtube={youtube} />
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
